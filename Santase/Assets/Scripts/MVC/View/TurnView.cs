c9852695c65e41f186d79b7d2b24d6ae2c975332
PCx200 <petko05raychinov@gmail.com>
2026-05-20 15:18:06 +0200

    Included fonts and refined the UI.

--- Santase/Assets/Scripts/MVC/View/TurnView.cs
+++ Santase/Assets/Scripts/MVC/View/TurnView.cs
@@ -3,10 +3,19 @@ using TMPro;
 
 public class TurnView : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI turnText;
+    [SerializeField] TextMeshProUGUI turnText1;
+    [SerializeField] private TextMeshProUGUI turnText2;
 
     public void UpdateTurn(int playerID)
     {
-        turnText.text = playerID == 0 ? "Player 1 Turn" : "Player 2 Turn";
+        GetTurnSpot(0).text = playerID == 0 ? "YOUR TURN" : "";
+        GetTurnSpot(1).text = playerID == 1 ? "YOUR TURN" : "";
+    }
+
+    private TextMeshProUGUI GetTurnSpot(int playerID)
+    {
+        if (GameController.Instance.localPlayerID == 0) return playerID == 0 ? turnText1 : turnText2;
+
+        else return playerID == 1 ? turnText1 : turnText2;
     }
 }
